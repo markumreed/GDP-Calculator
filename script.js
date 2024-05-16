f5f7a625b59fc31ec4332dddaf73c445b905baa9
Markum Reed <markumreed@gmail.com>
2024-05-16 10:11:07 -0400

    Finalize GDP Calculator project.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+// Create function for calculating GDP
 function gdpCalc(c,i,g,nx) {
 	const gdp = c+i+g+nx;
 	return gdp
 }
+
+// Using some test variables 
+const c = 10;
+const i = 20;
+const g = 120;
+const nx = -12;
+
+// Calling GDPCalc function
+const GDP = gdpCalc(c,i,g,nx);
+
+
+// Output to console
+console.log(`Total GDP: ${GDP}`);
